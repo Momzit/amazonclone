68408c79dbb66f97a7bb1a690d97facac3227f36
Moment <mhlvik001@myuct.ac.za>
2022-08-06 00:53:06 +0200

    First commit of the Amazon clone website, created header component, home component, and checkout component with its functionality

---
+++ src/components/Subtotal.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from './StateProvider'
+import './Subtotal.css'
+
+function Subtotal() {
+    const [{ basket }, ] = useStateValue();
+    
+    //This can be replaced with a reducer function that does the total in one line :)
+    const total = (basket) => {
+        let Sum = 0;
+        for (const product of basket) {
+            Sum += product.price
+        }
+        return Sum
+    }
+
+  return (
+    <div className='subtotal'>
+        <CurrencyFormat
+            renderText={(value) => (
+                <>
+                <p>Subtotal ({basket?.length} items) : 
+                    <strong> {`${ value }`}</strong>
+                </p>
+                <small className='subtotal__gift'>
+                    <input type='checkbox' />This order contains a gift
+                </small>
+                </>
+            )}
+            decimalScale={2}
+            value={total(basket)}
+            displayType={'text'}
+            thousandSeparator={true}
+            prefix={'R'}
+        />
+        <button>Proceed to checkout</button>
+    </div>
+  )
+}
+
+export default Subtotal
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBaYBauZW7u1xkLu0lElxhaaEFgMIAjTr0",
+    authDomain: "clone-project-b8fd5.firebaseapp.com",
+    projectId: "clone-project-b8fd5",
+    storageBucket: "clone-project-b8fd5.appspot.com",
+    messagingSenderId: "493495107361",
+    appId: "1:493495107361:web:6d9f26ea43b48a0be50860",
+    measurementId: "G-7D73VB4ZYX"
+  };
